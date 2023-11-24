8d99b4e8098bb9558f1dc3df2aee6c16962032c1
JignasaChavda <jignasha@sanskartechnolab.com>
2023-11-24 13:23:17 +0530

    Added some custom doctypes

---
+++ ambica_polymer/ambica_polymer/doctype/bus_facility_availment/test_bus_facility_availment.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Jignasa Chavda and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestBusFacilityAvailment(FrappeTestCase):
+	pass
